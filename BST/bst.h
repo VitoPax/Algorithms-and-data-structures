df6c966546971d91bc2dfa9a32a719b25a007f83
VitoPax <vitopace2001@gmail.com>
2026-04-28 18:01:54 +0200

    Studying bst

--- BST/bst.h
+++ BST/bst.h
@@ -1,17 +1,53 @@
-#ifndef BST_BST_H
-#define BST_BST_H
+#ifndef BST_H
+#define BST_H
 
 #include "item.h"
 
+/*
+ * Costanti per scegliere il tipo di visita.
+ */
+#define PREORDER  0
+#define INORDER   1
+#define POSTORDER 2
+
+/*
+ * ADT di prima classe.
+ *
+ * Fuori dal modulo bst.c non si vede com'è fatta davvero struct bst.
+ * L'utente usa solo il tipo BST.
+ */
 typedef struct bst *BST;
 
-BST BSTinit(void);
+/*
+ * Inizializzazione e deallocazione.
+ */
+BST  BSTinit(void);
 void BSTfree(BST bst);
+
+/*
+ * Operazioni di base.
+ */
+int  BSTcount(BST bst);
+int  BSTempty(BST bst);
 
 Item BSTsearch(BST bst, Key k);
 
+/*
+ * Inserimenti.
+ */
 void BSTinsert_leafR(BST bst, Item x);
+void BSTinsert_leafI(BST bst, Item x);
 void BSTinsert_root(BST bst, Item x);
 
+/*
+ * Minimo e massimo.
+ */
+Item BSTmin(BST bst);
+Item BSTmax(BST bst);
 
-#endif //BST_BST_H
+/*
+ * Visite.
+ */
+void BSTvisit(BST bst, int strategy);
+
+#endif
--- BST/main.c
+++ BST/main.c
@@ -1,9 +1,79 @@
 #include <stdio.h>
+#include "bst.h"
 
 int main(void) {
+    BST bst;
+    Item x;
+    Item found;
 
+    /*
+     * Creo un BST vuoto.
+     */
+    bst = BSTinit();
 
+    /*
+     * Inserimento in foglia ricorsivo.
+     */
+    x = (Item){"mario"};
+    BSTinsert_leafR(bst, x);
 
-    
+    x = (Item){"anna"};
+    BSTinsert_leafR(bst, x);
+
+    x = (Item){"zorro"};
+    BSTinsert_leafR(bst, x);
+
+    /*
+     * Inserimento in radice.
+     * "luca" verrà inserito e portato in radice tramite rotazioni.
+     */
+    x = (Item){"luca"};
+    BSTinsert_root(bst, x);
+
+    /*
+     * Visita inorder:
+     * in un BST stampa gli elementi in ordine crescente.
+     */
+    printf("Visita inorder: ");
+    BSTvisit(bst, INORDER);
+    printf("\n");
+
+    /*
+     * Ricerca.
+     */
+    found = BSTsearch(bst, "anna");
+
+    if (!ITEMcheckNull(found)) {
+        printf("Trovato: ");
+        ITEMstore(found);
+        printf("\n");
+    } else {
+        printf("Non trovato\n");
+    }
+
+    /*
+     * Minimo.
+     */
+    printf("Min: ");
+    ITEMstore(BSTmin(bst));
+    printf("\n");
+
+    /*
+     * Massimo.
+     */
+    printf("Max: ");
+    ITEMstore(BSTmax(bst));
+    printf("\n");
+
+    /*
+     * Numero di nodi.
+     */
+    printf("Numero nodi: %d\n", BSTcount(bst));
+
+    /*
+     * Deallocazione finale.
+     */
+    BSTfree(bst);
+
     return 0;
 }

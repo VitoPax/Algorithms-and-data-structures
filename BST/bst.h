1546c48a7fa08d7266b91644f5e725ad38ecedd2
VitoPax <vitopace2001@gmail.com>
2026-04-28 17:36:37 +0200

    Studying bst

--- BST/bst.h
+++ BST/bst.h
@@ -1,6 +1,17 @@
 #ifndef BST_BST_H
 #define BST_BST_H
 
+#include "item.h"
+
 typedef struct bst *BST;
+
+BST BSTinit(void);
+void BSTfree(BST bst);
+
+Item BSTsearch(BST bst, Key k);
+
+void BSTinsert_leafR(BST bst, Item x);
+void BSTinsert_root(BST bst, Item x);
+
 
 #endif //BST_BST_H

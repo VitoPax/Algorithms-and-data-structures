14ef2d7fcad4d5cb565858b61e30f3824424a795
VitoPax <vitopace2001@gmail.com>
2026-04-28 18:01:54 +0200

    Studying bst

--- BST/item.c
+++ BST/item.c
@@ -1,43 +1,89 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include "item.h"
 
-Item ITEMscan() {
+/*
+ * Legge una stringa da tastiera e la salva dentro val.name.
+ *
+ * %10s evita di leggere più di 10 caratteri,
+ * perché MAXC vale 11 e ci serve spazio per '\0'.
+ */
+Item ITEMscan(void) {
     Item val;
-    printf("name and value: ");
-    scanf("%s", val.name);
+
+    scanf("%10s", val.name);
+
     return val;
 }
 
-void ITEMstore(Item val) {
-    printf("name: %s\n", val.name);
+/*
+ * Crea un Item nullo.
+ *
+ * Mettiamo la stringa vuota "" dentro name.
+ * Quindi val.name[0] = '\0'.
+ */
+Item ITEMsetNull(void) {
+    Item val;
+
+    val.name[0] = '\0';
+
+    return val;
 }
 
+/*
+ * Un Item è nullo se la sua stringa è vuota.
+ */
 int ITEMcheckNull(Item val) {
-    if (strcmp(val.name, "")==0)
+    return val.name[0] == '\0';
+}
+
+/*
+ * Stampa l'Item solo se non è nullo.
+ */
+void ITEMstore(Item val) {
+    if (!ITEMcheckNull(val)) {
+        printf("%s", val.name);
+    }
+}
+
+/*
+ * Restituisce la chiave dell'Item.
+ *
+ * Qui la chiave coincide con name.
+ */
+Key KEYget(Item *pval) {
+    return pval->name;
+}
+
+/*
+ * Confronto tra due chiavi stringa.
+ *
+ * strcmp ritorna:
+ * - valore < 0 se k1 viene prima di k2
+ * - valore > 0 se k1 viene dopo k2
+ * - 0 se sono uguali
+ *
+ * Noi normalizziamo il risultato a -1, 0, 1.
+ */
+int KEYcmp(Key k1, Key k2) {
+    int cmp;
+
+    cmp = strcmp(k1, k2);
+
+    if (cmp < 0) {
+        return -1;
+    }
+
+    if (cmp > 0) {
         return 1;
+    }
+
     return 0;
 }
 
-Item ITEMsetNull() {
-    Item val = {""};
-    return val;
-}
-
-void KEYscan(Key k) {
-    scanf("%s", k);
-}
-
-Key KEYget(Item val) {
-    return val.name;
-}
-
-int KEYcmp(Key k1, Key k2){
-    if (strcmp(k1,k2)<0)
-        return -1;
-    else if (strcmp(k1,k2)>0)
-        return 1;
-    else
-        return 0;
+/*
+ * Legge una chiave da tastiera.
+ */
+void KEYscan(char *k) {
+    scanf("%10s", k);
 }

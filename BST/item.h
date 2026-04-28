c3b38b7f58d6df3db2e84f2fea271bc7603f996e
VitoPax <vitopace2001@gmail.com>
2026-04-28 18:01:54 +0200

    Studying bst

--- BST/item.h
+++ BST/item.h
@@ -1,24 +1,67 @@
-#ifndef BST_ITEM_H
-#define BST_ITEM_H
+#ifndef ITEM_H
+#define ITEM_H
 
 #include <stdio.h>
 
 #define MAXC 11
 
+/*
+ * In questo esempio:
+ * - la chiave è una stringa
+ * - l'Item contiene solo un campo name
+ *
+ * typedef const char *Key;
+ * significa che Key è un puntatore a carattere costante.
+ * In pratica useremo Key per confrontare le stringhe.
+ */
+typedef const char *Key;
+
 typedef struct {
     char name[MAXC];
 } Item;
 
-typedef char *Key;
+/*
+ * Legge un Item da tastiera.
+ */
+Item ITEMscan(void);
 
-Item    ITEMscan();
-Item    ITEMsetNull();
-int     ITEMcheckNull(Item val);
-void    ITEMstore(Item val);
-int     KEYcmp(Key k1, Key k2);
-Key     KEYget(Item *pval);
-void    KEYscan(Key k);
+/*
+ * Restituisce un Item "nullo".
+ * Lo usiamo quando una ricerca fallisce.
+ */
+Item ITEMsetNull(void);
 
+/*
+ * Controlla se un Item è nullo.
+ */
+int ITEMcheckNull(Item val);
 
+/*
+ * Stampa un Item.
+ */
+void ITEMstore(Item val);
 
-#endif //BST_ITEM_H
+/*
+ * Restituisce la chiave di un Item.
+ *
+ * Uso il puntatore Item *pval perché così evitiamo copie inutili
+ * e restiamo vicini allo stile ADT.
+ */
+Key KEYget(Item *pval);
+
+/*
+ * Confronta due chiavi.
+ *
+ * Ritorna:
+ * -1 se k1 < k2
+ *  0 se k1 == k2
+ *  1 se k1 > k2
+ */
+int KEYcmp(Key k1, Key k2);
+
+/*
+ * Legge una chiave da tastiera.
+ */
+void KEYscan(char *k);
+
+#endif

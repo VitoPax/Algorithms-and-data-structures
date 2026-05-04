811bcf3c8f76418fa7fb407843c15a32315082b5
VitoPax <vitopace2001@gmail.com>
2026-05-04 13:13:32 +0200

    Exam update

--- Esami/Giugno25/E01.c
+++ Esami/Giugno25/E01.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
-}
+/*
+    È dato un ADT List, in grado di rappresentare una lista (non ordinata) di stringhe (allocate dinamicamente).
+    Si completi la funzione listDeleteLonger che, data la lista e una lunghezza (un valore intero lmax),
+    elimini dalla lista le stringhe (e i relativi nodi) di lunghezza superiore a lmax.
+    La funzione ritorna un risultato intero, che rappresenta il numero di cancellazioni effettuate.
+    Completare il codice parziale utilizzando la numerazione riportata.
+*/

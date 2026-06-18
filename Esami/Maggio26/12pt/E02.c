4ec6c1318b700907701294db0d79eaf4bc16521c
VitoPax <vitopace2001@gmail.com>
2026-06-18 14:02:56 +0200

    Processiclear

---
+++ Esami/Maggio26/12pt/E02.c
@@ -0,0 +1,10 @@
+/*
+ È dato un tipo LIST (ADT di prima classe per liste concatenate semplici). Si riportano le definizioni dei tipi.
+
+ Si scriva una funzione che, data una lista LIST l, generi un duplicato (una seconda lista con contenente un
+ duplicato dei dati, nello stesso ordine). La funzione abbia prototipo
+
+ LIST LISTdup(LIST l);
+*/
+
+
---
+++ Esami/Maggio26/12pt/list.h
@@ -0,0 +1,7 @@
+#ifndef INC_12PT_LIST_H
+#define INC_12PT_LIST_H
+
+
+
+
+#endif //INC_12PT_LIST_H

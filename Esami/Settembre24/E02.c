8c1c0c8cae9d29007e0f62cdc4fcc4c1cc7cbd97
VitoPax <vitopace2001@gmail.com>
2026-09-11 11:12:13 +0200

    New exam initial release

---
+++ Esami/Settembre24/E02.c
@@ -0,0 +1,14 @@
+/*
+Sono dati due vettori di interi ordinati in modo crescente e privi di ripetizioni.
+Si scriva una funzione che generi un vettore (allocato dinamicamente) contenente gli interi appartenenti al primo vettore e non al secondo.
+La funzione deve essere chiamata come segue:
+
+c = diffVett(a,na,b,nb,&nc);
+
+a e b sono i due vettori, na e nb il numero di dati che contengono; c è il vettore risultato,
+allocato dinamicamente nella funzione, nc il numero di interi nel vettore risultato.
+
+Si richiede di realizzare (SOLO) la funzione diffVett (quindi non del programma chiamante).
+*/
+
+
